21cc4b0b96f8c5d31c7af40dc04e2db20b77d67a
Viktor Svyatokha <ahydrax@gmail.com>
2021-12-19 15:58:07 +1000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_mumble_telegram_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+dotnet-mumble-telegram-bot
